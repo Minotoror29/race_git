341fcaf9fd7de84ae40c79ca2ca2f5d31dc88688
Minotoror29 <matthieu.boissel@laposte.net>
2023-10-16 13:36:57 +0200

    Basic Ship Controls

--- Race/Assets/Scripts/ShipController.cs
+++ Race/Assets/Scripts/ShipController.cs
@@ -2,17 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class ShipController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody _rb;
+    private PlayerControls _playerControls;
+
+    [SerializeField] private float maximumSpeed;
+    private float _currentSpeed = 0f;
+
+    [SerializeField] private float rotationSpeed = 1f;
+    private float _rotationInput = 0f;
+
+    private void Start()
     {
-        
+        Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Initialize()
     {
-        
+        _rb = GetComponent<Rigidbody>();
+
+        _playerControls = new PlayerControls();
+        _playerControls.InGame.Enable();
+    }
+
+    private void Update()
+    {
+        HandleAccelerationInput();
+        HandleRotationInput();
+
+        Rotate();
+    }
+
+    private void HandleAccelerationInput()
+    {
+        if (_playerControls.InGame.Accelerate.ReadValue<float>() > 0)
+        {
+            _currentSpeed = maximumSpeed;
+        }
+        else
+        {
+            _currentSpeed = 0f;
+        }
+    }
+
+    private void HandleRotationInput()
+    {
+        _rotationInput = _playerControls.InGame.Rotate.ReadValue<Vector2>().x;
+    }
+
+    private void FixedUpdate()
+    {
+        Accelerate();
+    }
+
+    private void Accelerate()
+    {
+        _rb.velocity = transform.forward * _currentSpeed;
+    }
+
+    private void Rotate()
+    {
+        transform.Rotate(new Vector3(0, _rotationInput * rotationSpeed, 0));
     }
 }
