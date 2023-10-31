1e2b015fab292dcc58dc5f2a571bc245d77691bb
Minotoror29 <matthieu.boissel@laposte.net>
2023-10-31 18:37:16 +0100

    Text Mesh Pro + Boost System

--- Race/Assets/Scripts/ShipController.cs
+++ Race/Assets/Scripts/ShipController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
@@ -8,12 +9,17 @@ public class ShipController : MonoBehaviour
     private Rigidbody _rb;
     private PlayerControls _playerControls;
 
-    [SerializeField] private float maximumSpeed;
-    [SerializeField] private float brakeSpeed;
+    [SerializeField] private float maximumSpeed = 2500f;
+    [SerializeField] private AnimationCurve accelerationCurve;
+    [SerializeField] private TextMeshProUGUI speedMeter;
     private float _currentSpeed = 0f;
+    private float _accelerationTimer = 0f;
+
+    [SerializeField] private float boost1Speed = 5000f;
+    [SerializeField] private float boost2Speed = 7500f;
+    [SerializeField] private float boost3Speed = 10000f;
 
     [SerializeField] private float rotationSpeed = 1f;
-    [SerializeField] private float rotationBrakeSpeed = 1f;
     private float _rotationInput = 0f;
 
     public float CurrentSpeed { get { return _currentSpeed; } }
@@ -29,23 +35,30 @@ public class ShipController : MonoBehaviour
 
         _playerControls = new PlayerControls();
         _playerControls.InGame.Enable();
+        _playerControls.InGame.Boost.performed += ctx => Boost();
     }
 
     private void Update()
     {
         HandleAccelerationInput();
         HandleRotationInput();
-        HandleBoostInput();
+
+        speedMeter.text = ((int)_currentSpeed).ToString();
     }
 
     private void HandleAccelerationInput()
     {
         if (_playerControls.InGame.Accelerate.ReadValue<float>() > 0)
         {
-            _currentSpeed = maximumSpeed;
+            if (_accelerationTimer < accelerationCurve.keys[2].time)
+            {
+                _accelerationTimer += Time.deltaTime;
+                _currentSpeed = accelerationCurve.Evaluate(_accelerationTimer) * maximumSpeed;
+            }
         }
         else
         {
+            _accelerationTimer = 0f;
             _currentSpeed = 0f;
         }
     }
@@ -55,11 +68,19 @@ public class ShipController : MonoBehaviour
         _rotationInput = _playerControls.InGame.Rotate.ReadValue<Vector2>().x;
     }
 
-    private void HandleBoostInput()
+    private void Boost()
     {
-        if (_playerControls.InGame.Boost.ReadValue<float>() > 0)
+        Debug.Log("Boost");
+
+        if (_currentSpeed == maximumSpeed)
         {
-            _currentSpeed *= 2;
+            _currentSpeed = boost1Speed;
+        } else if (_currentSpeed == boost1Speed)
+        {
+            _currentSpeed = boost2Speed;
+        } else if (_currentSpeed == boost2Speed)
+        {
+            _currentSpeed = boost3Speed;
         }
     }
 
@@ -72,36 +93,10 @@ public class ShipController : MonoBehaviour
     private void Accelerate()
     {
         _rb.velocity = _currentSpeed * Time.fixedDeltaTime * transform.forward;
-
-        //if (_currentSpeed > 0f)
-        //{
-        //    _rb.AddForce(_currentSpeed * Time.fixedDeltaTime * transform.forward, ForceMode.Impulse);
-        //}
-
-        //Brake();
-
-        //Debug.Log(_rb.velocity.magnitude);
-    }
-
-    private void Brake()
-    {
-        _rb.velocity = Vector3.Lerp(_rb.velocity, Vector3.zero, brakeSpeed * Time.fixedDeltaTime);
     }
 
     private void Rotate()
     {
         transform.Rotate(new Vector3(0, _rotationInput * rotationSpeed, 0));
-
-        //if (_rotationInput != 0f)
-        //{
-        //    _rb.AddTorque(_rotationInput * rotationSpeed * Time.fixedDeltaTime * transform.up, ForceMode.Impulse);
-        //}
-        //StopAngularRotation();
-    }
-
-    private void StopAngularRotation()
-    {
-        _rb.angularVelocity = Vector3.Lerp(_rb.angularVelocity, Vector3.zero, rotationBrakeSpeed * Time.fixedDeltaTime);
-        //_rb.angularVelocity = Vector3.zero;
     }
 }
